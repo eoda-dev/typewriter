8746bdd991f036e5993b1741b966de6d29711b07
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-11-04 12:32:19 +0100

    Fix bug and add func to convert to list

--- R/helpers.R
+++ R/helpers.R
@@ -97,11 +97,25 @@ dump_by_alias <- function(obj, fields = NULL) {
     value <- obj[[name]]
     new_name <- ifelse(is.null(alias), name, alias)
     if (inherits(value, CLASS_RDANTIC)) {
-      l[[new_name]] <- by_alias(value)
+      l[[new_name]] <- dump_by_alias(value)
     } else {
       l[[new_name]] <- value
     }
   }
 
+  return(l)
+}
+
+# ---
+model_to_list <- function(obj) {
+  l = list()
+  for (name in names(obj)) {
+    value <- obj[[name]]
+    if(is.list(value)) {
+      l[[name]] <- model_to_list(value)
+    } else {
+      l[[name]] <- unclass(value)
+    }
+  }
   return(l)
 }
