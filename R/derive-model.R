28fcbcb23128ffafaad73d3d9b6ebeb4ce03c922
Nico Frieß <nico.friess@posteo.de>
2024-11-19 11:39:30 +0100

    add optionality to model derivation

--- R/derive-model.R
+++ R/derive-model.R
@@ -8,19 +8,30 @@
 #' @export
 model_from_template <- function(template,
                                 use_defaults = FALSE,
-                                use_length = FALSE) {
-  fields <- purrr::map(template, function(v) {
-    body <- substitute(
-      {
-        typeof(x) == vtype & class(x) == vclass & mode(x) == vmode
-      },
-      list(vtype = typeof(v), vclass = class(v), vmode = mode(v))
-    )
+                                use_length = FALSE,
+                                optional = FALSE) {
+  fields <- Map(function(v, nm){
+    if(isTRUE(optional) | nm %in% optional){
+      body <- substitute(
+        {
+          (typeof(x) == vtype & class(x) == vclass & mode(x) == vmode) | is.na(x)
+        },
+        list(vtype = typeof(v), vclass = class(v), vmode = mode(v))
+      )
+    }else {
+      body <- substitute(
+        {
+          typeof(x) == vtype & class(x) == vclass & mode(x) == vmode
+        },
+        list(vtype = typeof(v), vclass = class(v), vmode = mode(v))
+      )
+    }
     type_check_fn <- rlang::new_function(alist(x = ), body = body)
     return(model_field(
       fn = type_check_fn,
       default = ifelse(isTRUE(use_defaults), v, NA)
     ))
-  })
+  },
+  template, names(template))
   return(base_model(fields))
 }
