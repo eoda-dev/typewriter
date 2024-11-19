283b674452354e51d38f6638798c609d33b578d7
Nico Frieß <nico.friess@posteo.de>
2024-11-19 12:06:21 +0100

    documentation

--- examples/api/model-from-template.R
+++ examples/api/model-from-template.R
@@ -1,13 +1,24 @@
 template <- list(
   a = 10L,
-  b = 10L,
-  txt = "Some Text"
+  b = 10L
 )
-
-my_model <- model_from_template(template, use_defaults = TRUE)
-
 # Succeeds
-my_model(a = 20L)
+my_model(a = c(1:10))
 
 # Fails
 try(my_model(10))
+
+
+# Set values to optional
+my_model <- model_from_template(template, optional = TRUE)
+my_model(a = 12L) # returns NA for b
+
+# When optional is set to FALSE the model throws an error
+my_model <- model_from_template(template, optional = FALSE)
+try(my_model(a = 12L)) # returns NA for b
+
+
+# Use defaults if none provided
+my_model <- model_from_template(template, use_defaults = TRUE, optional = FALSE)
+my_model(a = 12L)
+
